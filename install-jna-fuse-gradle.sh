a6215cb61a7ad5c49c45ceb6462ab16fae7cdde7
vickykatara <vicky.p.katara@gmail.com>
2016-04-23 19:34:29 -0400

    sh files added

---
+++ install-jna-fuse-gradle.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo yum -y install java-1.8.0-openjdk-devel.x86_64
+wget https://downloads.gradle.org/distributions/gradle-2.12-bin.zip
+unzip gradle*
+cd gradle*/bin/
+export PATH=$PATH:$(pwd)
