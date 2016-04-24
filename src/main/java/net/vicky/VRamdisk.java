8c3795cf4f62024f00ab91c98739298bb6911fe2
vickykatara <vicky.p.katara@gmail.com>
2016-04-23 22:09:52 -0400

    Added

--- src/main/java/net/vicky/VRamdisk.java
+++ src/main/java/net/vicky/VRamdisk.java
@@ -228,7 +228,7 @@ public class VRamdisk extends net.fusejna.FuseFilesystem
 	@Override
 	public int open(final String path, final FileInfoWrapper info)
 	{
-		System.out.println("open called with " + path);
+		System.out.println("open called with " + path + " on " + this);
 		final int existingFD = openVFS.open_file(path);
 		if (existingFD != -1) {
 			info.fh(existingFD);
