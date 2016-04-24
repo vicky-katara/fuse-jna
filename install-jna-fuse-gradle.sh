23b74987b85c862c61e7708a185e2a80e6400aab
Vicky Katara <vicky.p.katara@gmail.com>
2016-04-24 13:23:52 -0400

    Update install-jna-fuse-gradle.sh

--- install-jna-fuse-gradle.sh
+++ install-jna-fuse-gradle.sh
@@ -3,7 +3,7 @@ sudo yum -y install java-1.8.0-openjdk-devel.x86_64
 wget https://downloads.gradle.org/distributions/gradle-2.12-bin.zip
 unzip gradle*
 cd gradle*/bin/
-gradlePath = $(pwd)
+gradlePath=$(pwd)
 cd /usr/bin
 sudo ln -s $gradlePath gradle
 cd ~/fuse-jna/
