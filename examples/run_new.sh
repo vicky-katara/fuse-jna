140d8d61626d1dd9697a1bbe045e856ee19a6615
vickykatara <vicky.p.katara@gmail.com>
2016-04-24 23:25:45 -0400

    asdad

--- examples/run_new.sh
+++ examples/run_new.sh
@@ -12,25 +12,14 @@ if ! which gradle &> /dev/null; then
   exit 1
 fi
 
-#check if dir exists else create
-n=1
-mountPoint=${!n}
+mountPoint="${@: -1}"
 if [ ! -d "$mountPoint" ]; then
 	mkdir -p "$mountPoint"
 fi
-
-#echo the number, second argument
-n=2
-# echo "number" ${!n}
-number=${!n}
-#file
-n=3 
-# echo "file" ${!n}
-filename=${!n}
-
 absoluteMountpoint="$(cd "$mountPoint" && pwd)"
 set -- "${@:1:$(expr "$#" - 1)}" "$absoluteMountpoint"
 
 cd "$(dirname "$BASH_SOURCE")/.."
+
 GRADLE_USER_HOME=gradle gradle uberJar
-java -cp build/libs/\* "$mountpoint $number $filename"
+java -cp build/libs/\* "$@"
